9e88d71079d1160dbeb19ffd9aaec78e17c79b17
mrazomej <manuel.razo.m@gmail.com>
2017-07-05 15:45:36 -0700

    modify code to compile IPTG titrations and lacI titration into different files

--- code/processing/compile_flow_data.py
+++ code/processing/compile_flow_data.py
@@ -2,11 +2,20 @@ import glob
 import pandas as pd
 
 #============================================================================== 
-# list directories
-files = glob.glob('*flow/output/*.csv')
+# list directories with IPTG titration
+files = glob.glob('*flow/output/*IPTG*.csv')
 
 # Import them as a single data frame
 df = pd.concat(pd.read_csv(f, comment='#') for f in files)
 
 # Export data frame as flow_master.csv
 df.to_csv('../../data/flow_master.csv')
+
+# list directories with lacI titration
+files = glob.glob('*flow/output/*lacI*.csv')
+
+# Import them as a single data frame
+df = pd.concat(pd.read_csv(f, comment='#') for f in files)
+
+# Export data frame as flow_master.csv
+df.to_csv('../../data/flow_lacI_master.csv')
