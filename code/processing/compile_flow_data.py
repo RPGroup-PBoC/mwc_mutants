2e4e2e888fce3f6c488754d0579a5a9009f9c57a
mrazomej <manuel.razo.m@gmail.com>
2017-07-06 19:30:21 -0700

    change the column of the mutant class name to avoid conflicts

--- code/processing/compile_flow_data.py
+++ code/processing/compile_flow_data.py
@@ -7,6 +7,8 @@ files = glob.glob('*flow/output/*IPTG*.csv')
 
 # Import them as a single data frame
 df = pd.concat(pd.read_csv(f, comment='#') for f in files)
+# Rename the column class to mut_class to avoid conflicts
+df.rename(columns={'class': 'mut_class'}, inplace=True)
 
 # Export data frame as flow_master.csv
 df.to_csv('../../data/flow_master.csv')
@@ -16,6 +18,9 @@ files = glob.glob('*flow/output/*lacI*.csv')
 
 # Import them as a single data frame
 df = pd.concat(pd.read_csv(f, comment='#') for f in files)
+# Rename the column class to mut_class to avoid conflicts
+df.rename(columns={'class': 'mut_class'}, inplace=True)
 
 # Export data frame as flow_master.csv
 df.to_csv('../../data/flow_lacI_master.csv')
+
