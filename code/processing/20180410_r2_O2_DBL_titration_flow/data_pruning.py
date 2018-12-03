f6203f0751b382fdd02bb7479a3a1d1974ee6f4d
Griffin Chure <gchure@caltech.edu>
2018-12-03 13:56:00 -0800

    More changes processing files. Removing more examples of incorrectly considered data

---
+++ code/processing/20180410_r2_O2_DBL_titration_flow/data_pruning.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+# Get the csv file. 
+file = pd.read_csv('output/20180410_r2_DBL_fold_change.csv')
+
+# Drop the spurious concentration
+file = file[file['mutant'] != 'Y20I-Q294K']
+
+# Resave. 
+file.to_csv('output/20180410_r2_DBL_fold_change.csv')
---
+++ code/processing/20180413_r1_O2_DBL_titration_flow/data_pruning.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+# Get the csv file. 
+file = pd.read_csv('output/20180413_r1_DBL_fold_change.csv')
+
+# Drop the spurious concentration
+file = file[file['mutant'] != 'Y20I-Q294K']
+
+# Resave. 
+file.to_csv('output/20180413_r1_DBL_fold_change.csv')
