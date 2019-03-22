7e3ba9e007cadb70849ad817c3d697a95db4ce03
Griffin Chure <gchure@caltech.edu>
2019-03-21 19:48:00 -0700

    More progress on the interactive sensitivity explorer

--- code/figures/js/sensitivity.js
+++ code/figures/js/sensitivity.js
@@ -47,44 +47,31 @@ function derivKi(ep, c, ka, kiValue, n) {
 
 // Define the parameter values
 var data = source.data;
-var ka = ka.value;
-var ki = ki.value;
-var epAI = epAI.value;
-var c = c.value;
-var n = nSites.value;
+var ka = Ka.value;
+var ki = Ki.value;
+var epAI = EpAI.value;
+var c = C.value;
+var n = nSites;
 
-// Enforce positivity for log-scaled variables. 
-if (xKa.start <= 0) {
-    xKa.start = 0.00000002
-}
-if (xKa.end <= 0) {
-    xKa.end = 0.00000002
-}
-if (xKi.start <= 0) {
-    xKi.start = 0.00000002
-}
-if (xKi.end <= 0) {
-    xKi.end = 0.00000002
-}
-if (xC.start <= 0) {
-    xC.start = 0.00000002
-}
-if (xC.end <= 0) {
-    xC.end = 0.00000002
-}
 // Define the observed ranges
-data['epAI'] = linspace(xepAI.start, xepAI.end, n_points); 
-data['ka'] = linspace(xKa.start, xKa.end, n_points);
-data['ki'] = linspace(xKi.start, xKi.end, n_points);
-data['c'] = linspace(xC.start, xC.end, n_points);
+// var range_epAI = linspace(xepAI.start, xepAI.end, n_points); 
+// data['epAI']  = range_epAI;
+// var range_ka = linspace(xKa.start, xKa.end, n_points);
+// data['ka'] = range_ka;
+// var range_ki = linspace(xKi.start, xKi.end, n_points);
+// data['ki'] = range_ki;
+// var range_c = linspace(xC.start, xC.end, n_points);
+// data['c']  = range_c;
+
 
 
 // Call the functions to compute the derivatives
 for (var i = 0; i < n_points; i++) {
-   data['dF_depAI'][i] = derivEpAI(data['epAI'][i], c, ka, ki, n);
-   data['dF_dc'][i] = derivC(epAI, data['c'][i], ka, ki, n);
-   data['dF_dka'][i] = derivKa(epAI, c, data['ka'][i], ki, n);
-   data['dF_dki'][i] = derivKi(epAI, c, ka, data['ki'][i], n);}
+   data['dF_depAI'][i] = derivEpAI(xepAI[i], c, ka, ki, n);
+   data['dF_dc'][i] = derivC(epAI, xC[i], ka, ki, n);
+   data['dF_dka'][i] = derivKa(epAI, c, xKa[i], ki, n);
+   data['dF_dki'][i] = derivKi(epAI, c, ka, xKi[i], n);}
+
 
 // Update the sources. 
 source.change.emit()
--- code/figures/js/sensitivity.py
+++ code/figures/js/sensitivity.py
@@ -13,8 +13,38 @@ from bokeh.models.widgets import Select, Slider, RadioButtonGroup, Button,  Rang
 pboc = mut.viz.color_selector('pboc')
 bokeh.plotting.output_file("sensitivity.html")
 
+
+# Define python functions for computing the WT parameter sensitivities
+def deriv_epai(ep_value, c, ka, ki, n):
+    allo_ratio = ((1 + c/ka) / (1 + c/ki))**n
+    return (1 + np.exp(ep_value) * allo_ratio)**-1
+
+def deriv_c(ep, c_value, ka, ki, n):
+    numer = n * ((ki * (ka + c_value)) / (ka * (ki + c_value)))**n
+    denom_a = (ka + c_value) * (ki + c_value)
+    denom_b = ((ki * (ka  c_value) / (ka * (ki + c_value))))**n + np.exp(ep)
+    return numer / (denom_a * denom_b)
+
+def deriv_ka(ep, c, ka_value, ki, n):
+    numer = c * n * (ki * (ka_value + c) / (ka_value * (ki + c)))**n
+    denom_a = ka_value * (ka_value + c)  
+    denom_b = ((ki * (ka + c)) / (ka * (ki + c)))**n + np.exp(ep)
+    return numer / (denom_a * denom_b)
+
+def deriv_ki(ep, c, ka, ki_value, n):
+    numer = -c * n * ((ki_value * (ka + c)) / (ka * (ki_value + c)))**n
+    denom_a = ki_value * (ki_value + c) 
+    denom_b = ((ki_value * (ka+c)) / (ka * (ki_value + c)))**n + np.exp(ep)
+    return numer / (denom_a * denom_b)
+
 # Define the parameter ranges
-n_points = 500
+n_points = 1000
+
+# Don't permit adjustable ranges
+ka_range = np.logspace(-2, 3, n_points)
+ki_range = np.logspace(-2, 3, n_points)
+epAI_range = np.linspace(-10, 10, n_points)
+c_range = np.logspace(-3, 3, n_points)
 
 # Define the sliders
 ka_slider = Slider(title='Ka [µM]', value=139, start=0.01, end=1000, step=0.01)
@@ -23,126 +53,65 @@ epAI_slider = Slider(title='allosteric energy difference [kT]', value=4.5,
                     start=-10, end=10, step=0.1)
 c_slider = Slider(title='IPTG [µM]', value=50, start=0.01, end=5000, step=0.1)
 
+# Define the range sliders
+
 # Define the source data 
-source = ColumnDataSource(data={'ka':np.ones(n_points), 'ki':np.ones(n_points), 
-                                'epAI':np.arange(0, n_points, 1), 'c':np.ones(n_points),
-                                'dF_depAI':np.arange(0, n_points, 1), 
+source = ColumnDataSource(data={'ka':ka_range, 'ki':ki_range,
+                                'epAI':epAI_range, 'c':c_range,
+                                'dF_depAI':np.arange(1, n_points+1, 1), 
                                 'dF_dka':np.ones(n_points), 'dF_dki':np.ones(n_points), 
                                 'dF_dc':np.ones(n_points)})
 
 # Instantiate the figures 
 p_epAI = bokeh.plotting.figure(width=425, height=300, 
                                x_axis_label='allosteric energy difference [kT]',
-                               y_axis_label='∂∆F / ∂∆ε_AI')
+                               y_axis_label='∂∆F / ∂∆ε_AI',
+                               y_range=[0.5, 1.5])
 p_Ka = bokeh.plotting.figure(width=425, height=300, 
                             x_axis_type='log', x_axis_label = 'Ka [µM]',
-                            y_axis_label='∂∆F / ∂Ka')
+                            y_axis_label='∂∆F / ∂Ka' ,
+                            y_range=[-.1, .1])
 p_Ki = bokeh.plotting.figure(width=425, height=300, 
                             x_axis_type='log', x_axis_label = 'Ki [µM]',
-                            y_axis_label='∂∆F / ∂Ki')
+                            y_axis_label='∂∆F / ∂Ki',
+                            y_range=[-1, 1])
 p_c = bokeh.plotting.figure(width=425, height=300, 
                             x_axis_type='log', x_axis_label = 'IPTG [µM]',
-                            y_axis_label='∂∆F / ∂c')
+                            y_axis_label='∂∆F / ∂c',
+                            y_range=[-1, 1])
 
 # Add initial glyphs. 
 p_epAI.line(x='epAI', y='dF_depAI', source=source, color=pboc['red'], line_width=2)
-p_Ka.line(x='ka', y='ka', source=source, color=pboc['red'], line_width=2)
-p_Ki.line(x='ki', y='ki', source=source, color=pboc['red'], line_width=2)
-p_c.line(x='c', y='c', source=source, color=pboc['red'], line_width=2)
+p_Ka.line(x='ka', y='dF_dka', source=source, color=pboc['red'], line_width=2)
+p_Ki.line(x='ki', y='dF_dki', source=source, color=pboc['red'], line_width=2)
+p_c.line(x='c', y='dF_dc', source=source, color=pboc['red'], line_width=2)
 
 # Define callback arguments
-callback_args = {'source':source, 'nPoints':n_points, 'ka':ka_slider, 'ki':ki_slider,
-                 'epAI':epAI_slider, 'c':c_slider, 'nSites':2,
-                 'xKa':p_Ka.x_range, 'xKi':p_Ki.x_range, 'xepAI':p_epAI.x_range,
-                 'xC':p_c.x_range, 'n_points':n_points}
+callback_args = {'source':source, 'nPoints':n_points, 'Ka':ka_slider, 'Ki':ki_slider,
+                 'EpAI':epAI_slider, 'C':c_slider, 'nSites':2,
+                 'xKa':ka_range, 'xKi':ki_range, 'xepAI':epAI_range,
+                 'xC':c_range, 'n_points':n_points, 'nSites':2}
 
 
 # Define the callback definitions
-def callback_gen(cb_list, args=callback_args):
-   return CustomJS(args=args, code=" ".join(cb_list)) 
-
-
-fn_callback = """
-    // Define the parameter values
-    var data = source.data;
-    var ka = ka.value;
-    var ki = ki.value;
-    var epAI = epAI.value;
-    var c = c.value;
-    var n = nSites.value;
-
-    function linspace(start, stop, n) {
-        var x = [];
-        var currValue = start;
-        var step = (stop - start) / (n - 1);
-        for (var i = 0; i < n; i++) {
-            x.push(currValue);
-            currValue += step;
-        }
-        return x;
-    }
-
-    function logspace(start, stop, n) {
-        var x = [];
-        var currValue = start;
-        var step = (stop - start) / n;
-        for (var i = 0; i < n; i++) {
-            x.push(Math.pow(10, currValue));
-            currValue += step   
-        }
-    }
-
-    function derivEpAI(epValue, c, ka, ki, n){
-        var allo_ratio =  Math.pow(1 + c / ka, n) / Math.pow(1 + c/ki, n);
-        return Math.pow(1 + Math.exp(epValue) * allo_ratio, -1);
-    }
-
-    function derivC(ep, cValue,  ka, ki, n){
-        var numer = n * Math.pow((ki * (ka + cValue))/(ka * (ki + cValue)),n) * (ka - ki);
-        var denom_a = (ka + cValue) * (ki + cValue);
-        var denom_b = (Math.pow((ki * (ka + cValue) / (ka * (ki + cValue))), n) + Math.exp(ep));
-        return numer / (denom_a * denom_b);
-    }
-
-    function derivKa(ep, c, kaValue, ki, n) {
-        var numer = c * n * Math.pow((ki * (kaValue + c))/ (kaValue * (ki + c)), n);
-        var denom_a = kaValue * (kaValue + c)
-        var denom_b = Math.pow((ki * (ka + c))/ (ka * (ki + c)), n) + Math.exp(ep);
-        return numer / (denom_a * denom_b);
-    }
-
-    function derivKi(ep, c, ka, kiValue, n) { 
-        var numer = -c * n * Math.pow((kiValue * (ka + c))/ (ka * (kiValue + c)), n);
-        var denom_a = kiValue * (kiValue + c)
-        var denom_b = Math.pow((kiValue * (ka + c)) / (ka * (kiValue + c)), n)  + Math.exp(ep)
-        return numer / (denom_a * denom_b);
-    }
-
-    """
-dF_depAI_callback = """
-    data['epAI'] = linspace(xepAI.start, xepAI.end, n_points); 
-    for (var i = 0; i < n_points; i++) {
-   data['dF_depAI'][i] = derivEpAI(data['epAI'][i], c, ka, ki, n);}
-   source.change.emit()
-    """
-df_dKa_callback = """
-    data['ka'] = linspace(xKa.start, xKa.end, n_points);
-    """
-# with open("./sensitivity.js") as f:
-    # callback_str = f.read()
-# callback = CustomJS(args=callback_args, code=callback_str)
+with open("sensitivity.js") as f:
+    callback = CustomJS(args=callback_args, code=f.read())
 
 # Trigger callbacks
 sliders = [epAI_slider, ka_slider, ki_slider, c_slider]
-plots = [p_epAI, p_Ka, p_Ki, p_epAI]
+plots = [p_epAI, p_Ka, p_Ki, p_c]
 for p, s in zip(plots, sliders):
-    p.x_range.callback = callback_gen([fn_callback, dF_depAI_callback])
-    s.js_on_change('value', callback_gen([fn_callback, dF_depAI_callback]))
+    # p.x_range.range_padding  = 0
+    # p.x_range.callback = callback
+    s.callback = callback
 
 # Generate the layout
 params = [epAI_slider, ka_slider, ki_slider, c_slider]
 widgets = widgetbox(params)
-
+# ka_row = bokeh.layouts.column(p_Ka, ka_range)
+# epAI_row = bokeh.layouts.column(p_epAI, epAI_range)
+# ki_row = bokeh.layouts.column(p_Ki, ki_range)
+# c_row = bokeh.layouts.column(p_c, c_range)
 layout  = bokeh.layouts.layout([[params], 
                                 [p_Ka, p_Ki], [p_epAI, p_c]])
 
