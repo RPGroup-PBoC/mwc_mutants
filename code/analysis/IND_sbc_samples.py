b64ef0bb774b2712a4567174abcb987e9500a5ad
Griffin Chure <gchure@caltech.edu>
2019-04-18 08:12:51 -0700

    update

--- code/analysis/IND_sbc_samples.py
+++ code/analysis/IND_sbc_samples.py
@@ -62,7 +62,7 @@ def sbc(g, d):
             
         # Sample the model
         model = _model[g[0]]
-        _, samples = model.sample(data_dict=data_dict,  iter=10000, n_jobs=2, chains=4, control=dict(adapt_delta=0.999))
+        _, samples = model.sample(data_dict=data_dict,  iter=2000, n_jobs=1, chains=4)
         samples.rename(columns=columns, inplace=True)
         samples['sim_idx'] = g[1]
         samples['model'] = g[0]
