a183e21910007f58f8756be50874e16d7c68324a
Griffin Chure <gchure@caltech.edu>
2019-05-06 17:53:23 -0700

    Added bokeh plot, working on resizing

---
+++ code/figures/js/DNA_muts_deltaF.py
@@ -0,0 +1,51 @@
+# ##############################################################################
+# DELTA F FIGURE AXES
+# ##############################################################################
+Y20I_R60 = bokeh.plotting.figure(width=250, height=200,
+                                x_axis_label='reference free energy [kT]',
+                                y_axis_label='∆F [kT]',
+                                title='Y20I - R = 60')
+Y20I_R124 = bokeh.plotting.figure(width=250, height=200,
+                                x_axis_label='reference free energy [kT]',
+                                y_axis_label='∆F [kT]',
+                                title='Y20I - R = 124')
+Y20I_R260 = bokeh.plotting.figure(width=250, height=200,
+                                x_axis_label='reference free energy [kT]',
+                                y_axis_label='∆F [kT]',
+                                title='Y20I - R = 260')
+Y20I_R1220 = bokeh.plotting.figure(width=250, height=200,
+                                x_axis_label='reference free energy [kT]',
+                                y_axis_label='∆F [kT]',
+                                title='Y20I - R = 1220')
+Q21A_R60 = bokeh.plotting.figure(width=250, height=200,
+                                x_axis_label='reference free energy [kT]',
+                                y_axis_label='∆F [kT]',
+                                title='Q21A - R = 60')
+Q21A_R124 = bokeh.plotting.figure(width=250, height=200,
+                                x_axis_label='reference free energy [kT]',
+                                y_axis_label='∆F [kT]',
+                                title='Q21A - R = 124')
+Q21A_R260 = bokeh.plotting.figure(width=250, height=200,
+                                x_axis_label='reference free energy [kT]',
+                                y_axis_label='∆F [kT]',
+                                title='Q21A - R = 260')
+Q21A_R1220 = bokeh.plotting.figure(width=250, height=200,
+                                x_axis_label='reference free energy [kT]',
+                                y_axis_label='∆F [kT]',
+                                title='Q21A - R = 1220')
+Q21M_R60 = bokeh.plotting.figure(width=250, height=200,
+                                x_axis_label='reference free energy [kT]',
+                                y_axis_label='∆F [kT]',
+                                title='Q21M - R = 60')
+Q21M_R124 = bokeh.plotting.figure(width=250, height=200,
+                                x_axis_label='reference free energy [kT]',
+                                y_axis_label='∆F [kT]',
+                                title='Q21M - R = 124')
+Q21M_R260 = bokeh.plotting.figure(width=250, height=200,
+                                x_axis_label='reference free energy [kT]',
+                                y_axis_label='∆F [kT]',
+                                title='Q21M - R = 260')
+Q21M_R1220 = bokeh.plotting.figure(width=250, height=200,
+                                x_axis_label='reference free energy [kT]',
+                                y_axis_label='∆F [kT]',
+                                title='Q21M - R = 1220')
