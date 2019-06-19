2533ed292b182092a3c172dac3a4bacb3659f3a3
Griffin Chure <gchure@caltech.edu>
2019-06-18 19:50:02 -0700

    Finished interactive figure for parameter selection

--- code/figures/js/param_selector.js
+++ code/figures/js/param_selector.js
@@ -1,60 +1,203 @@
-// Define data sources
-var o1 = o1_source.data;
-var o2 = o2_source.data;
-var o3 = o3_source.data;
-
 // Define the constants;
 var inducer = c_range;
 var nns = 4600000;
 var n = 2;
+var operators = ['O1', 'O2', 'O3', 'Oid']
+var energies = [-15.3, -13.9, -9.7, -17]
 var repressors = [22, 60, 124, 260, 1220, 1740];
 
 // Define sliders and inputs, and 
 var input_option = Radio.active; 
+var drop_option = Drop.value;
+var description = Desc;
+var epri = 0;
 
 if  (input_option == 0 ) {
     var ka_val = parseFloat(Ka_numer.value); 
     var ki_val = parseFloat(Ki_numer.value); 
     var krr_val = parseFloat(Krr_numer.value); 
+    var epri = 0
 
 }
 
-else {
+else if (input_option == 1) {
     var ka_val = Ka_slider.value; 
     var ki_val = Ki_slider.value;
     var krr_val = Krr_slider.value; 
+    var epri = 0
+}
+
+else {
+    if (drop_option == 'razo') {
+        var ka_val = 139;
+        var ki_val = 0.53;
+        var krr_val = 0.01
+        description.text = "<b> Razo-Mejia <i>et al.</i>, Cell Systems, 2018</b><br/> Ka = 139 µM<br/> Ki = 0.53 µM<br/> K_RR*=0.01<br/> Inactive repressor DNA binding is negligible";
+        var epri = 0;
+    }
+
+    else if (drop_option == 'one_not_enough_x0') {
+        var ka_val = 60;
+        var ki_val = 4.2; 
+        var krr_val = 2.3;
+        description.text = "<b>Daber, Sharp, and Lewis. J. Mol. Biol. 2009</b><br/>Ka = 60 µM<br/> Ki = 4 µM<br/> K_RR* = 2.3<br/> Inactive repressor DNA binding is negligible"
+        var epri = 0
+    }
+
+    else if (drop_option == 'one_not_enough_x100000') {
+        var ka_val = 60;
+        var ki_val = 4; 
+        var krr_val = 2.3;
+        var epri = 1 / 100000;
+        description.text = "<b>Daber, Sharp, and Lewis. J. Mol. Biol. 2009</b><br/>Ka = 60 µM<br/> Ki = 4 µM<br/> K_RR* = 2.3<br/> Inactive repressor DNA binding 1 / 100,000 strength of active"
+    }
+
+    else if (drop_option == 'one_not_enough_x10000') {
+        var ka_val = 60;
+        var ki_val = 4; 
+        var krr_val = 2.4;
+        var epri = 1 / 10000;
+        description.text = "<b>Daber, Sharp, and Lewis. J. Mol. Biol. 2009</b><br/>Ka = 60 µM<br/> Ki = 4 µM<br/> K_RR* = 2.4<br/> Inactive repressor DNA binding 1 / 10,000 strength of active"
+    }
+
+    else if (drop_option == 'one_not_enough_x1000') {
+        var ka_val = 79;
+        var ki_val = 4; 
+        var krr_val = 1.3;
+        var epri = 1 / 1000;
+        description.text = "<b>Daber, Sharp, and Lewis. J. Mol. Biol. 2009</b><br/>Ka = 79 µM<br/> Ki = 4 µM<br/> K_RR* = 2.4<br/> Inactive repressor DNA binding 1 / 1000 strength of active"
+    }
+
+    else if (drop_option == 'one_not_enough_x100') {
+        var ka_val = 812;
+        var ki_val = 0.3; 
+        var krr_val = 0.01;
+        var epri = 1 / 100;
+        description.text = "<b>Daber, Sharp, and Lewis. J. Mol. Biol. 2009</b><br/>Ka = 79 µM<br/> Ki = 4 µM<br/> K_RR* = 0.01<br/> Inactive repressor DNA binding 1 / 100 strength of active"
+    }
+
+    else if (drop_option == 'daber_muts')  {
+        var ka_val =  16;
+        var ki_val = 2;
+        var krr_val = 5.8;
+        var epri = 0
+        description.text = "<b> Daber, Sochor, and  Lewis. J. Mol. Biol. 2011</b><br/>Ka = 16 µM<br/> Ki = 2 µM<br/> K_RR* = 5.8<br/> Inactive repressor DNA binding is negligible"
+    }
+
+    else if (drop_option == 'ogorman') {
+        var ka_val = 133;
+        var ki_val = 4;
+        var krr_val = 0.7;
+        var epri = 1 / 1000;
+        description.text = "<b>O'Gorman <i> et al.</i>, JBC, 1980</b><br/>Ka = 133 µM<br/> Ki = 4 µM<br/> K_RR* = 0.7<br/> Inactive repressor DNA binding 1 / 1000 strength of active"
+    }
 }
 
 // Define the function for computing the fold-change
-function foldChange(R, epr) {
+function foldChange(R, epr, epri) {
     fc = []
+    if (epri != 0) {
+        var pref = 1;
+    }
+    else {
+        var pref = 0
+    }
     for (var i = 0; i < inducer.length; i++) {
     var numer = Math.pow(1 + inducer[i] / ka_val, n);
     var denom = numer + krr_val * Math.pow(1 + inducer[i] / ki_val, n);
-    fc[i] =  1 / ( 1 + (numer / denom) * (R / nns) * Math.exp(-epr));
+    fc[i] =  1 / (1 + (numer / denom) * (R / nns) * Math.exp(-epr) + pref * (1 - (numer / denom)) * (R / nns) * Math.exp(-epri * epr));
     }
     return fc;
 }
 
 function updateFoldChange(source, epr) {
-    var xs = [];
     var ys = [];
-    var cs = []
+    var cs = [];
     for (var i = 0; i < repressors.length; i++) {
-        xs[i] = inducer;
-        ys[i] = foldChange(repressors[i], epr);
+        ys[i] = foldChange(repressors[i], epr, epri);
         cs[i] = repressors[i];
     }
-    console.log(ys)
-    source.data['IPTGuM'] = xs;
     source.data['fold_change'] = ys;
-    return source
+    source.change.emit()
 }
 
+function updateLeakiness(source) {
+    var ys=[]; 
+    var pact = 1 / (1 + krr_val);
+        if (epri != 0) {
+            var pref = 1;
+        }
+        else {
+            var pref = 0;
+        }
+    for (var i =0 ; i < operators.length; i++) {
+        var _ys = []
+        for (var j=0; j < rep_range.length; j++) {
+            var ra = rep_range[j] * pact / nns;
+            var ri = rep_range[j] * (1 - pact) / nns;
+
+            _ys[j] = 1 / (1 + ra * Math.exp(-energies[i]) + pref * ri * Math.exp(-epri * energies[i]));
+            console.log(_ys[j])
+    }
+    ys[i] = _ys;
+    }
+    // source.data['repressors'] = xs;
+    source.data['leakiness'] = ys;
+    source.change.emit();
+}
+
+
+// Define function to compute the fugacity. 
+function computePact() {
+    var numer = Math.pow(1 + 0 / ki_val, n);
+    var denom = Math.pow(1 + 0 / ka_val, n);
+    var prob = Math.pow(1 + krr_val* numer / denom, -1);
+    return prob;
+    }
+
+function computeFugacity(pact, ep,  epri, M) {
+    var fug = [];
+    if (epri != 0) {
+        var pref = 1;
+    }
+
+    else {
+        var pref = 0;
+    }
+    var x = Math.exp(15.3);
+    var xi = Math.exp(-epri * (-15.3));
+    for (var j = 0; j < rep_range.length; j++) {
+
+        var ra = rep_range[j] * pact;
+        var ri = rep_range[j] * (1 - pact);
+        var B_act = (ra * (1 + x) - M * x - nns) 
+        var B_inact = (ri * (1 + xi) - M * xi - nns) 
+        var C_act = ra;
+        var C_inact = ri;
+        var A_act = x * (ra - M - nns);
+        var A_inact = xi * (ri - M - nns);
+        var numer_act = -B_act - Math.sqrt(B_act*B_act - 4 * A_act * C_act);
+        var numer_inact = -B_inact - Math.sqrt(B_inact*B_inact - 4 * A_inact * C_inact);
+        var denom_act = 2 * A_act;
+        var denom_inact = 2 * A_inact;
+        fug[j] = 1 / (1 + (numer_act / denom_act) * x + pref * (numer_inact /denom_inact) * xi) 
+ }
+ return fug;
+}
+
+var M = [64, 52]
+function updateFoldChangeFugacity(source) {
+    var pact = 1 / (1 + krr_val);
+    ys = [];
+    for (var j = 0; j < M.length; j++) {
+        ys[j] = computeFugacity(pact, -15.3, epri, M[j]);
+    }
+    source.data['leakiness'] = ys;
+    source.change.emit();
+    }
 // Define the three sources
-o1_source = updateFoldChange(o1_source, -15.3);
-o2_source = updateFoldChange(o2_source, -13.9);
-o3_source = updateFoldChange(o3_source, -9.7);
-o1_source.change.emit();
-o2_source.change.emit();
-o3_source.change.emit();
+updateFoldChange(o1_source, -15.3, epri);
+updateFoldChange(o2_source, -13.9, epri);
+updateFoldChange(o3_source, -9.7, epri);
+updateLeakiness(leak_source);
+updateFoldChangeFugacity(fug_source);
--- code/figures/js/parameter_value_picker.py
+++ code/figures/js/parameter_value_picker.py
@@ -6,17 +6,25 @@ import numpy as np
 import pandas as pd
 import bokeh.io
 import bokeh.plotting
-from bokeh.models.widgets import TextInput, Slider, Toggle, Button, RadioButtonGroup
+from bokeh.models.widgets import TextInput, Slider, Toggle, Button, RadioButtonGroup, Dropdown, Div
 from bokeh.models import CustomJS, ColumnDataSource
 import bokeh.transform
 import bokeh.palettes
 import bokeh.layouts
 import mut.thermo
 constants = mut.thermo.load_constants()
+constants['Oid'] = -17
 bokeh.plotting.output_file("param_selector.html")
 
 # Load the data set (Razo-Mejia et al 2018)
 data = pd.read_csv('../../../data/csv/RazoMejia2018_data.csv')
+data = data.sort_values(['repressors'])
+old_gods = pd.read_csv('../../../data/csv/Garcia2011_Brewster2014_data.csv')
+garcia = old_gods[old_gods['author']=='garcia']
+brewster = old_gods[old_gods['author']=='brewster']
+fug_data = pd.read_csv('../../../data/csv/fugacity_data.csv')
+fug_data = fug_data[fug_data['operator']=='O1']
+fug_data['N'] = fug_data['N'].astype(str)
 
 # Clean the data
 data = data[data['repressors'] > 0]
@@ -32,6 +40,9 @@ pal = bokeh.transform.factor_cmap('repressors', palette=bokeh.palettes.Dark2_6,
 O1_data = data[data['operator']=='O1']
 O2_data = data[data['operator']=='O2']
 O3_data = data[data['operator']=='O3']
+garcia_source = ColumnDataSource(garcia)
+brewster_source = ColumnDataSource(garcia)
+fug_source = ColumnDataSource(fug_data)
 
 # Define the raw data sets as sources
 O1_summ = O1_data.groupby(['repressors', 
@@ -59,7 +70,7 @@ ka_init = 139
 ki_init = 0.53
 krr_init = np.exp(-4.5)
 sources = []
-for o in data['operator'].unique():
+for o in ['O1', 'O2', 'O3', 'Oid']:
     xs = []
     ys = []
     cs = []
@@ -68,12 +79,44 @@ for o in data['operator'].unique():
                                         ep_ai=4.5, effector_conc=c_range).fold_change()
         xs.append(c_range)
         ys.append(fc)
-        cs.append(r)
+        cs.append([r])
     sources.append({'IPTGuM': xs, 'fold_change':ys, 'repressors':cs})
-
 O1_theo = ColumnDataSource(sources[1])
 O2_theo = ColumnDataSource(sources[0])
 O3_theo = ColumnDataSource(sources[2])
+
+# Leakiness plots
+rep_range = np.logspace(0, 4, 100)
+xs = []
+ys = []
+cs = []
+for o in ['O1', 'O2', 'O3', 'Oid']:
+    xs.append(rep_range)
+    pact = (1 + np.exp(-constants['ep_AI']))
+    Ra = pact * rep_range / 4.6E6
+    ys.append((1 + Ra * np.exp(-constants[o]))**-1)
+    cs.append(o)
+leak_theo = ColumnDataSource({'repressors':xs, 'leakiness':ys, 'operator':cs})
+
+pal2 = bokeh.transform.factor_cmap('repressors', palette=bokeh.palettes.Dark2_6, 
+                                   factors=data.repressors.unique())
+
+# Compute the fugacity data
+xs = []
+ys = []
+cs = []
+for n in [64, 52]:
+    xs.append(rep_range)
+    x = np.exp(15.3)
+    pact = 1 / (1 + np.exp(-constants['ep_AI']))
+    r = pact * rep_range
+    B = rep_range * (1 + x) - n * x - 4.6E6
+    A = x * (r - n - 4.6E6)
+    numer = -B - np.sqrt(B * B - 4 * A * r)
+    denom = 2 * A
+    ys.append(1 / (1 + (numer / denom) * np.exp(15.3)))
+    cs.append(str(n))
+fug_theo = ColumnDataSource({'repressors':xs, 'leakiness':ys, 'N':cs})
 
 #%%
 # Instantiate the direct text inputs
@@ -84,10 +127,29 @@ krr_input = TextInput(value="0.001", title='K_RR* (exp{-βΔε})')
 # Instantiate the slider inputs
 ka_slider = Slider(start=0, end=500, step=0.01, value=139, title='Ka [µM]')
 ki_slider = Slider(start=0, end=500, step=0.01, value=0.53, title='Ki [µM]')
-krr_slider = Slider(start=-10, end=10, step=0.01, value=0.01, title='K_RR* (exp{-βΔε})')
+krr_slider = Slider(start=0.000001, end=10, step=0.01, value=0.01, title='K_RR* (exp{-βΔε})')
 
 # Instantiate the selector buttons
-selector = RadioButtonGroup(labels=['Use Numeric Input', 'Use Slider Input'], active=0)
+selector = RadioButtonGroup(labels=['Use Numeric Input', 'Use Slider Input', 'Use Literature Values'], active=0)
+drop = Dropdown(label='Select Literature Source', menu=[('Razo-Mejia et al. 2018', 'razo'), 
+                                                        ('Daber, Sharp, and Lewis 2009 (Δε_RI = 0)', 'one_not_enough_x0'),
+                                                        ('Daber, Sharp, and Lewis 2009 (Δε_RI = Δε_RA / 100,000)', 'one_not_enough_x100000'),
+                                                        ('Daber, Sharp, and Lewis 2009 (Δε_RI = Δε_RA / 10,000)', 'one_not_enough_x10000'),
+                                                        ('Daber, Sharp, and Lewis 2009 (Δε_RI = Δε_RA / 1000)', 'one_not_enough_x1000'),
+                                                        ('Daber, Sharp, and Lewis 2009 (Δε_RI = Δε_RA / 100)', 'one_not_enough_x100'),
+                                                        ('Daber, Sochor, and Lewis 2011', 'daber_muts'),
+                                                        ("O'Gorman et al. 1980", 'ogorman')],
+                                                        button_type='warning')
+
+# Describe the parameter values.
+vals = Div(text="""Literature values appear here when selection is made.""", width =300, height=150)
+div1 = Div(text="""
+<b>This interactive figure allows you to explore how changing parameter values of the simple repression thermodynamic model match <i>in vivo</i> data collected in the Phillips Group. To use, first select the mode of entry (Numeric, Slider, or from Literature Values) followed by your selection of parameters. The data should update as soon as a selection is made.</b>
+""", width = 900, height=50)
+div2 = Div(text=""" <center><b>These data come from Razo-Mejia <i>et al.</i> 2018 in which
+the fold-change in gene expression was measured <i> in vivo</i>.</b></center>""", width=900, height=75)
+div3 = Div(text=""" <center><b>These data come Garcia and Phillips 2011 and Brewster <i>et al.</i> 2014. They show the fold-change in gene expression measured for LacI tetramers (circles) and dimers (diamonds) for four different operators in the absence of inducer. In these experiments, there is a single specific binding site in the genome. the fold-change in gene expression was measured <i> in vivo</i>.</b></center>""", width=450, height=75)
+div4 = Div(text="""<center><b>These data come from Brewster <i>et al.</i> 2014 in which the fold-chang in gene expression for a simple repression architecture with the native O1 operator sequence was present in the cell with either 64 (green) or 52 (orange) copies of the promoter.</b></center>""", width=400, height=75)
 
 # Execute button
 execute = Button(label='Update Plots', button_type='success')
@@ -97,7 +159,9 @@ box1 = bokeh.layouts.widgetbox([ka_input, ki_input,
                                  krr_input])
 box2 = bokeh.layouts.widgetbox([ka_slider, ki_slider, 
                                 krr_slider])
-row1 = bokeh.layouts.row(box1, box2)
+
+box3 = bokeh.layouts.widgetbox([drop, vals])
+row1 = bokeh.layouts.row(box1, box2, box3)
 widget_layout = bokeh.layouts.column(selector, row1)
 
 
@@ -109,7 +173,10 @@ callback = CustomJS(args=dict(o1_source=O1_theo, o2_source=O2_theo,
                             o3_source=O3_theo, Ka_numer=ka_input, 
                             Ki_numer=ki_input, Krr_numer=krr_input,
                             Ka_slider=ka_slider, Ki_slider=ki_slider, 
-                            Krr_slider=krr_slider, Radio=selector, c_range=c_range), code=cb)
+                            Krr_slider=krr_slider, Radio=selector, 
+                            c_range=c_range, Drop=drop, Desc=vals, 
+                            rep_range=rep_range, leak_source=leak_theo,
+                            fug_source=fug_theo), code=cb)
 
 # Apply the callback to the update plots
 ka_input.callback = callback
@@ -124,6 +191,7 @@ krr_slider.callback = callback
 ka_slider.js_on_change('value', callback)
 ki_slider.js_on_change('value', callback)
 krr_slider.js_on_change('value', callback)
+drop.js_on_change("value", callback)
 
 # Set up the figure canvas
 o1_ax = bokeh.plotting.figure(width=300, height=300, x_axis_type='log',
@@ -135,6 +203,14 @@ o2_ax = bokeh.plotting.figure(width=300, height=300, x_axis_type='log',
 o3_ax = bokeh.plotting.figure(width=300, height=300, x_axis_type='log',
                             x_axis_label='IPTG [µM]', y_axis_label='fold-change',
                             title='Operator O3 : Δε_RA = -9.7 kT')
+old_ax = bokeh.plotting.figure(width=450, height=300, 
+                                  x_axis_type='log', y_axis_type='log',
+                                  x_axis_label='repressors per cell', y_axis_label='leakiness',
+                                  title="Single promoter, no inducer")
+fug_ax = bokeh.plotting.figure(width=450, height=300, 
+                                  x_axis_type='log', y_axis_type='log',
+                                  x_axis_label='repressors per cell', y_axis_label='leakiness',
+                                  title='variable promoter copy number (O1 operator)')
 
 # Plot the theory
 o1_ax.multi_line(xs='IPTGuM', ys='fold_change', line_width=1, source=O1_theo, line_color=pal)
@@ -152,15 +228,34 @@ o3_ax.segment(x0='IPTGuM', x1='IPTGuM', y0='low', y1='high', line_width=1.5,
                 source=O3_summ, line_color=pal)
 o3_ax.circle(x='IPTGuM', y='mean', source=O3_summ, fill_color='white', line_color=pal, size=5)
 
+# Plot the brewster and garcia data
+pal3 = bokeh.transform.factor_cmap(field_name='operator', palette=bokeh.palettes.Dark2_6, factors=['O1', 'O2', 'O3', 'Oid'])
+pal4 = bokeh.transform.factor_cmap(field_name='N', palette=bokeh.palettes.Dark2_6, factors=fug_data['N'].unique())
+old_ax.circle(x='repressor', y='fold_change', fill_color='white', 
+            legend='operator', line_color=pal3, size=6, source=garcia_source)
+old_ax.diamond(x='repressor', y='fold_change', fill_color='white', line_color=pal3, size=8, source=brewster)
+fug_ax.circle(x='repressor', y='fold_change', source=fug_source, fill_color='white', 
+             line_color=pal4, size=6, legend='N')
+
+# Plot the Leakiness and fugacity theory
+old_ax.multi_line(xs='repressors', ys='leakiness', color=pal3, source=leak_theo, line_width=1)
+fug_ax.multi_line(xs='repressors', ys='leakiness', color=pal4, source=fug_theo, line_width=1)
+
 # Format the legends
 o1_ax.legend.location = 'top_left'
 o1_ax.legend.title = 'R'
+old_ax.legend.location = 'bottom_left'
+fug_ax.legend.title = 'number of promoters'
+fug_ax.legend.location = 'bottom_left'
 
 # Define the plot layouts
-plots = bokeh.layouts.row(o1_ax, o2_ax, o3_ax)
-layout = bokeh.layouts.column(widget_layout, plots)
+razo_plots = bokeh.layouts.row(o1_ax, o2_ax, o3_ax)
+old_god_plots = bokeh.layouts.row(old_ax, fug_ax)
+div_row = bokeh.layouts.row(div3, div4)
+layout = bokeh.layouts.column(div1, widget_layout, razo_plots, old_god_plots)
+# bokeh.io.show(layout)
+
 bokeh.io.save(layout)
-#%%
 
 
 #%%
